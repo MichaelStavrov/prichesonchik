c124f4c06d7c0666be83204bda6f284a12f598c3
Michael <mihali5@mail.ru>
2025-01-18 15:10:42 +0300

    add metrika script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -209,8 +209,9 @@ export default function RootLayout({
             id='metrika-counter'
             strategy='afterInteractive'
             type='text/javascript'
-          >
-            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            src='../utils/metrika.js'
+          />
+          {/* {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
               m[i].l=1*new Date();
               for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -222,8 +223,8 @@ export default function RootLayout({
                     trackLinks:true,
                     accurateTrackBounce:true,
                     webvisor:true
-            });`}
-          </Script>
+            });`} */}
+
           <noscript>
             <div>
               <Image
