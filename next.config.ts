24b86d29bcb216d863c698d65f97bb08dc9afd77
Michael <mihali5@mail.ru>
2026-02-20 21:09:38 +0300

    удалил ненужное

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  // output: 'export',
+  // images: { unoptimized: true },
+  // trailingSlash: true,
+};
 
 export default nextConfig;
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export const UserRoleEnum = {
-  REGULAR: 'REGULAR',
-  CLIENT: 'CLIENT',
-  ADMIN: 'ADMIN',
-} as const;
-
-export type UserRole = (typeof UserRoleEnum)[keyof typeof UserRoleEnum];
